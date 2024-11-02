6c3e068c410466ba1686f43d03cc726d6ef29a79
viktorortelan <jvortelan@gmail.com>
2024-11-02 14:37:54 -0300

    skills pronta

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -34,6 +34,69 @@ export default function Home () {
                 <img src="/assets/images/viktor.jpg" alt="" />
                 </div>
             </div>
+
+            <div className="seccao2">
+                <div className="protecao">
+                    <h1>-SKILLS</h1>
+                    <div className="carde1">
+                        <div className="card">
+                            <img src="/assets/images/bxl-html5 1.png" alt="" />
+                            <h1>HTML</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/bxl-css3 1.png" alt="" />
+                            <h1>CSS</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/bxl-react 1.png" alt="" />
+                            <h1>REACT-JS</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/bxl-sass 1.png" alt="" />
+                            <h1>SASS</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/bxl-nodejs 1.png" alt="" />
+                            <h1>NODE-JS</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/bxl-javascript 1.png" alt="" />
+                            <h1>JAVASCRIPT</h1>
+                        </div>
+                    </div>
+                    <div className="carde2">
+                        <div className="card">
+                            <img src="/assets/images/server.png" alt="" />
+                            <h1>SQL SERVER</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/workbe.png" alt="" />
+                            <h1>SQL WORCKBENCH</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/aws.png" alt="" />
+                            <h1>AWS</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/giit.png" alt="" />
+                            <h1>GIT</h1>
+                        </div>
+                        <div className="card">
+                            <img src="/assets/images/git.png" alt="" />
+                            <h1>GITHUB</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="seccao3">
+                <div className="protecao">
+                    <h1>-PROJETOS</h1>
+                    <div className="cartao">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
